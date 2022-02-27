62113379c867064cf6c7a25070b027faa203362e
mrkresnofatih <90560422+mrkresnofatih@users.noreply.github.com>
2022-02-27 22:16:58 +0700

    updated env logger at startup

--- Startup.cs
+++ Startup.cs
@@ -40,7 +40,7 @@ namespace FavoriteFoodApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
@@ -48,6 +48,8 @@ namespace FavoriteFoodApi
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FavoriteFoodApi v1"));
             }
+            
+            logger.LogInformation($"Environment is {Configuration["EnvName"]}.");
             
             app.UseAppExceptionHandler();
 
